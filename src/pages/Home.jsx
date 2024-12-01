a871b9ccd781130a2b6dcf98d2eb765ad6588b60
Michael Gauthier <mikegauthier222@hotmail.com>
2024-12-01 15:46:29 -0800

    reposition logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import logo from '../assets/images/home-page/logoedit.png';
 export default function Home(handleRequestClick) {
   return (
     <div id="home">
-      <div className="content-wrapper flex flex-col items-center justify-center pt-60 text-center sm:pt-40">
-        <img src={logo} alt="logo" className="w-1/3 sm:w-1/5"/>
+      <div className="content-wrapper flex flex-col items-center justify-center pt-60 text-center sm:pt-36">
+        <img src={logo} alt="logo" className="mb-4 w-1/2 sm:w-1/5"/>
         <div>
 
           <h1 className="text-4xl md:text-5xl text-white font-black">
