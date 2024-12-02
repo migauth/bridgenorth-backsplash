824a35bf8312a327455ea6135afa775369725ebc
Michael Gauthier <mikegauthier222@hotmail.com>
2024-12-02 13:29:10 -0800

    redo layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,14 @@ import logo from '../assets/images/home-page/logoedit.png';
 
 export default function Home(handleRequestClick) {
   return (
-      <div className="content-wrapper flex flex-col items-center justify-center pt-60 text-center bg-blue-gray-400md:flex-row  lg:flex-col sm:pt-36">
+      <div className="content-wrapper flex flex-col items-center justify-center pt-60 text-center md:flex-row  lg:flex-col sm:pt-36">
         <img src={logo} alt="logo" className="mb-4 w-1/2 sm:w-1/5"/>
         <div>
           <div>
           <h1 className="text-2xl sm:text-2xl md:text-4xl  lg:text-5xl text-white ">
             Your local backsplash expert
           </h1>
-          <h2 className="sm:text-xl md:text-2xl md:font-boldlg:text-3xl text-white mt-4">
+          <h2 className="sm:text-xl md:text-2xl md:font-bold lg:text-3xl text-white mt-4">
             Since 1985
           </h2>
 
