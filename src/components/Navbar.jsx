c7e963684cb6a3de447518519deef99530c35978
Michael Gauthier <mikegauthier222@hotmail.com>
2024-12-03 09:10:10 -0800

    arrow hides nav bar and reveals

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,50 +32,65 @@ const Navbar = () => {
   };
 
   return (
-    <div
-      className={`navbar fixed w-full flex flex-wrap items-center justify-between p-6 text-xl text-white ${
-        isScrolled ? "scrolled" : "at-top"
-      } ${isNavbarVisible ? "" : "hidden"}`}
-    >
-      <div className="w-full md:w-auto text-center md:text-left">
-        <h1
-          id="logo"
-          className="sm:text-xl lg:text-2xl"
-          onClick={() => scrollToSection()}
-        >
-          Bridgenorth Backsplash
-        </h1>
+    <>
+      <div
+        className={`navbar fixed w-full flex flex-wrap items-center justify-between p-6 text-xl text-white ${
+          isScrolled ? "scrolled" : "at-top"
+        } ${isNavbarVisible ? "" : "hidden"}`}
+      >
+        <div className="w-full md:w-auto text-center md:text-left">
+          <h1
+            id="logo"
+            className="sm:text-xl lg:text-2xl"
+            onClick={() => scrollToSection()}
+          >
+            Bridgenorth Backsplash
+          </h1>
+        </div>
+        <div className="sanch text-sm flex gap-2 w-full justify-center md:w-auto md:gap-4 md:justify-end mt-4 md:mt-0 lg:text-xl">
+          <button
+            className="hover:text-gray-400"
+            onClick={() => scrollToSection("about")}
+          >
+            About
+          </button>
+          <button
+            className="hover:text-gray-400"
+            onClick={() => scrollToSection("gallery")}
+          >
+            Gallery
+          </button>
+          <button
+            className="hover:text-gray-400"
+            onClick={() => scrollToSection("testimonials")}
+          >
+            Testimonials
+          </button>
+          <button
+            className="hover:text-gray-400"
+            onClick={() => scrollToSection("contact")}
+          >
+            Contact
+          </button>
+          <button
+            className="hover:text-gray-400 hover:animate-bounce"
+            onClick={handleNavbarToggle}
+          >
+            <i className="fa-solid fa-angles-up"></i>
+          </button>
+        </div>
       </div>
-      <div className="sanch text-sm flex gap-2 w-full justify-center md:w-auto md:gap-4 md:justify-end mt-4 md:mt-0 lg:text-xl">
-        <button
-          className="hover:text-gray-400"
-          onClick={() => scrollToSection("about")}
-        >
-          About
-        </button>
-        <button
-          className="hover:text-gray-400"
-          onClick={() => scrollToSection("gallery")}
-        >
-          Gallery
-        </button>
-        <button
-          className="hover:text-gray-400"
-          onClick={() => scrollToSection("testimonials")}
-        >
-          Testimonials
-        </button>
-        <button
-          className="hover:text-gray-400"
-          onClick={() => scrollToSection("contact")}
-        >
-          Contact
-        </button>
-        <button className="hover:text-gray-400 hover:animate-bounce" onClick={handleNavbarToggle}>
-          <i className="fa-solid fa-angles-up"></i>
-        </button>
-      </div>
-    </div>
+      {!isNavbarVisible && (
+        <div className="navbar fixed w-full flex flex-row-reverse p-6 text-xl text-white">
+          <button
+            className="hover:text-gray-400 hover:animate-bounce"
+            onClick={handleNavbarToggle}
+          >
+            <i className="fa-solid fa-angles-up"></i>
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
